54cb03eb98bcbaf842f1ddeea8ef538645cb1109
zaberov.dv <zaberov.dv@internet.ru>
2024-03-21 20:40:50 +0300

    Домашняя работа 25.1 - миграции

--- users/serializers/user.py
+++ users/serializers/user.py
@@ -1,3 +1,4 @@
+from django import forms
 from rest_framework import serializers
 
 from lms.models import Course
@@ -10,6 +11,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        """Скрыть пароль в профиле"""
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
+
 
 class UserSerializerList(serializers.ModelSerializer):
     payer = PaymentSerializer(many=True, read_only=True)
@@ -17,3 +24,11 @@ class UserSerializerList(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+# class UserLimitedSerializer(serializers.ModelSerializer):
+#     """Исключает отображение пароля и фамилии"""
+#
+#     class Meta:
+#         model = User
+#         exclude = ('password', 'last_name',)
--- users/views/user.py
+++ users/views/user.py
@@ -1,19 +1,28 @@
 from rest_framework import generics
+from rest_framework.response import Response
+
 from users.models import User
-from users.serializers.user import UserSerializer, UserSerializerList
+from users.permissions import IsOwner
+from users.serializers.user import UserSerializer, UserSerializerList, UserLimitedSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
 class UserSerializeListAPIView(generics.ListAPIView):
     serializer_class = UserSerializerList
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset = super().get_queryset(*args, **kwargs)
+    #     if self.request.user.id != queryset.get(pk=self.request.user.id):
+    #         queryset = queryset.exclude('password', 'last_name',)
+    #     return queryset
 
 
 class UserSerializeRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UserSerializeCreateAPIView(generics.CreateAPIView):
@@ -29,9 +38,9 @@ class UserSerializeCreateAPIView(generics.CreateAPIView):
 class UserSerializeUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UserSerializeDestroyAPIView(generics.DestroyAPIView):
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
